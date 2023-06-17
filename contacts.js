c31b7b5d0bcd1c6c3823c4ca76487181762128bc
olena-nikitchenko <helnikitka@gmail.com>
2023-06-18 01:42:25 +0300

    added new files and change old files

--- contacts.js
+++ contacts.js
@@ -2,47 +2,44 @@ const fs = require('fs/promises');
 const path = require('path');
 const { nanoid } = require('nanoid');
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const contactsPath = path.join(__dirname, 'db/contacts.json');
 
-
-const listContacts = async() => {
-  const data = await fs.readFile(contactsPath);
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath);
     return JSON.parse(data);
-}
+};
 
-const getContactById = async(contactId) => {
-      const contacts = await listContacts();
+const getContactById = async contactId => {
+    const contacts = await listContacts();
     const result = contacts.find(item => item.id === contactId);
     return result || null;
-  }
+};
 
-const removeContact = async (contactId) => {
+const removeContact = async contactId => {
     const contacts = await listContacts();
     const index = contacts.findIndex(item => item.id === contactId);
-    if(index === -1) {
+    if (index === -1) {
         return null;
     }
     const [result] = contacts.splice(index, 1);
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-return result;
-  }
+    return result;
+};
 
-const addContact = async(name, email, phone) =>{
+const addContact = async data => {
     const contacts = await listContacts();
     const newContact = {
         id: nanoid(),
-        name,
-        email,
-        phone
-    }
-  contacts.push(newContact);
-await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-return newContact;
-}
+        ...data,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+};
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-    addContact
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
 };
